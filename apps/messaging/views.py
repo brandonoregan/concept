18a14e0b173cafe4b3641e0c269faa6fe7017165
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-11-29 15:01:39 +0000

    add basic function of retrieving conversation messages

--- apps/messaging/views.py
+++ apps/messaging/views.py
@@ -5,15 +5,39 @@ from .forms import MessageForm
 from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
+
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
 
+# When the inbox view is called I want to display the most recent message they've recieved
+# When message is clicked on and form is submitted I want to display that specific message 
+
+# Get the most recent message and all preceding messages where the sender is the current user and the reciever id is equal to the sender
+
+
 
 def inbox(request):
-    user_messages = Message.objects.filter(receiver=request.user)
+    current_user = request.user
+    # Store all message where the current user is the receiver 
+    user_messages = Message.objects.filter(receiver=current_user).order_by('-id')
+    
+    # Get the most recent message received
+    if user_messages:
+        display_message = user_messages[0]
+        display_message.sender
+
+        message_thread = Message.objects.filter(Q(sender=current_user, receiver=display_message.sender) | Q(sender=display_message.sender, receiver=current_user)).order_by('sent_at')
+
+        return render(request, "messaging/inbox.html", {
+        "user_messages": user_messages,
+        "display_message": display_message,
+        "message_thread": message_thread,
+        })
+        
 
     # Retrieve the message_id from the URL parameters
     message_id = request.GET.get("message_id")
@@ -26,7 +50,9 @@ def inbox(request):
         )
         # Now 'message' object is available in the inbox view if message_id is present in the URL
 
-    return render(request, "messaging/inbox.html", {"user_messages": user_messages})
+    return render(request, "messaging/inbox.html", {
+        "user_messages": user_messages,
+        })
 
 
 class CreateMessage(LoginRequiredMixin, CreateView):
