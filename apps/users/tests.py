7b0f7f2268a4cdb33137f9daff2d7ded6c3b723f
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-12-19 15:09:44 +0000

    remove postgresql db and switch to sqlite

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class URLTests(TestCase):
+    def test_login(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
--- concept/settings.py
+++ concept/settings.py
@@ -30,12 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    "concept-env-v5.eba-jmnuwas7.eu-west-1.elasticbeanstalk.com",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -94,13 +91,9 @@ WSGI_APPLICATION = "concept.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": os.environ.get("USER"),
-        "PASSWORD": os.environ.get("PASSWORD"),
-        "HOST": "concept-db.cpfatwfjoswc.eu-west-1.rds.amazonaws.com",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Adjust this path as needed
     }
 }
 
