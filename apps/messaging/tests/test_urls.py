1fca3409c05aa6bdf00ac8949090162d90d4d5b2
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-12-28 16:55:21 +0000

    update messaging test, add apps.messaging url tests

---
+++ apps/messaging/tests/test_urls.py
@@ -0,0 +1,24 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve 
+from apps.messaging.views import inbox, create_message
+
+class TestUrls(SimpleTestCase):
+
+    def test_inbox_url_resolves(self):
+        url = reverse('inbox')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, inbox)
+
+    def test_inbox_with_user_url_resolves(self):
+        username = 'testuser'
+        url = reverse('inbox_with_user', kwargs={'user_username': username})
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, inbox)
+        self.assertEqual(resolve(url).kwargs['user_username'], username)
+    
+    
+    def test_create_message_url_resolves(self):
+        url = reverse('create_message')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, create_message)
+
