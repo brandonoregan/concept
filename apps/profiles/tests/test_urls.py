b15ec09acaf33955321a99c0f309e7c3f7c53e7f
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-12-29 21:06:29 +0000

    add profiles url resolve tests

--- apps/profiles/tests/test_urls.py
+++ apps/profiles/tests/test_urls.py
@@ -1,24 +1,23 @@
-# from django.test import SimpleTestCase
-# from django.urls import reverse, resolve 
-# from apps.messaging.views import inbox, create_message
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve 
+from apps.profiles.views import profile, edit_profile_picture, edit_profile_bio, edit_user
 
-# class TestUrls(SimpleTestCase):
+class TestUrls(SimpleTestCase):
 
-#     def test_inbox_url_resolves(self):
-#         url = reverse('inbox')
-#         print(resolve(url))
-#         self.assertEqual(resolve(url).func, inbox)
+    def test_profile_url_resolves(self):
+        url = reverse("profile")
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, profile)
 
-#     def test_inbox_with_user_url_resolves(self):
-#         username = 'testuser'
-#         url = reverse('inbox_with_user', kwargs={'user_username': username})
-#         print(resolve(url))
-#         self.assertEqual(resolve(url).func, inbox)
-#         self.assertEqual(resolve(url).kwargs['user_username'], username)
+    def test_edit_profile_picture_url_resolves(self):
+        self.assertEqual(resolve(reverse("edit_profile_picture")).func, edit_profile_picture)
+
+    def test_edit_profile_bio_url_resolves(self):
+        self.assertEqual(resolve(reverse("edit_profile_bio")).func, edit_profile_bio)
+
+    def test_edit_user_url_resolves(self):
+        self.assertEqual(resolve(reverse('edit_user')).func, edit_user)
+
+
     
-    
-#     def test_create_message_url_resolves(self):
-#         url = reverse('create_message')
-#         print(resolve(url))
-#         self.assertEqual(resolve(url).func, create_message)
 
