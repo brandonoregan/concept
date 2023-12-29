fa1df3d032f5247512e23d1faf4149ca513669ff
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-12-29 21:18:45 +0000

    add users url resolves test

--- apps/users/tests/test_urls.py
+++ apps/users/tests/test_urls.py
@@ -1,24 +1,28 @@
-# from django.test import SimpleTestCase
-# from django.urls import reverse, resolve 
-# from apps.messaging.views import inbox, create_message
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve 
+from apps.users.views import welcome, LoginUser, RegisterUser, LogoutUser
+from django.contrib.auth.views import PasswordResetDoneView, PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView
 
-# class TestUrls(SimpleTestCase):
+class TestUrls(SimpleTestCase):
 
-#     def test_inbox_url_resolves(self):
-#         url = reverse('inbox')
-#         print(resolve(url))
-#         self.assertEqual(resolve(url).func, inbox)
+    def test_welcome_url_resolves(self):
+        self.assertEqual(resolve(reverse('welcome')).func, welcome)
 
-#     def test_inbox_with_user_url_resolves(self):
-#         username = 'testuser'
-#         url = reverse('inbox_with_user', kwargs={'user_username': username})
-#         print(resolve(url))
-#         self.assertEqual(resolve(url).func, inbox)
-#         self.assertEqual(resolve(url).kwargs['user_username'], username)
-    
-    
-#     def test_create_message_url_resolves(self):
-#         url = reverse('create_message')
-#         print(resolve(url))
-#         self.assertEqual(resolve(url).func, create_message)
+    def test_login_user_url_resolves(self):
+        self.assertEqual(resolve(reverse('login_user')).func.view_class, LoginUser)
+
+    def test_resgister_url_resolves(self):
+        self.assertEqual(resolve(reverse('register_user')).func.view_class, RegisterUser)
+
+    def test_logout_url_resolves(self):
+        self.assertEqual(resolve(reverse('logout_user')).func.view_class, LogoutUser)
+
+    def test_password_reset_url_resolves(self):
+        self.assertEqual(resolve(reverse('password-reset')).func.view_class, PasswordResetView)
+
+    def test_password_reset_done_url_resolves(self):
+        self.assertEqual(resolve(reverse('password_reset_done')).func.view_class, PasswordResetDoneView)
+
+    def test_password_reset_complete_url_resolves(self):
+        self.assertEqual(resolve(reverse('password_reset_complete')).func.view_class, PasswordResetCompleteView)
 
